310fb0baf3f2f764207f367139939728d26910d3
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2024-12-28 15:25:22 -0600

    Added comments.

--- Day1/one_one.py
+++ Day1/one_one.py
@@ -1,16 +1,23 @@
+# path to input file
 file = open(r"F:\Users\prest\Documents\Projects\AdventOfCode2024\Day1\input.txt")
 
 def find_distance(file):
+    # placeholder for distance value
     distance = 0
+    # read input file into list split by newlines
     lines = file.readlines()
 
+    # create a 1x2 list of pairs on each line split by white space and strip the newline characters
     pairs = [[int(id.strip()) for id in line.split("   ")] for line in lines]
 
+    # create two new lists, one of the left column of pairs and one of the right column of pairs
     ids1, ids2 = [pair[0] for pair in pairs], [pair[1] for pair in pairs]
 
+    # sort ascending order
     ids1.sort()
     ids2.sort()
 
+    # create a rolling sum of the difference between ids in the left column and right column
     for i, id in enumerate(ids1):
         distance += abs(id - ids2[i])
 
--- Day1/one_two.py
+++ Day1/one_two.py
@@ -1,14 +1,20 @@
+# path to input file
 file = open(r"F:\Users\prest\Documents\Projects\AdventOfCode2024\Day1\input.txt")
 
 def find_similarity(file):
+    # placeholder for similarity value
     score = 0
+    # read input file into list split by newlines
     lines = file.readlines()
 
+    # create a 1x2 list of pairs on each line split by white space and strip the newline characters
     pairs = [[int(id.strip()) for id in line.split("   ")] for line in lines]
 
+    # create two new lists, one of the left column of pairs and one of the right column of pairs
     ids1, ids2 = [pair[0] for pair in pairs], [pair[1] for pair in pairs]
 
-    for i, id in enumerate(ids1):
+    # create a rolling sum of the ids in the left list multiplied by how often they occur in the right list
+    for id in ids1:
         score += id * ids2.count(id)
 
     print(score)
