dc93d3e95207976a35bec52befa4afefce1fc29e
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2024-12-29 16:31:53 -0600

    Start to 3-1 challenge

---
+++ Day3/three_one.py
@@ -0,0 +1,13 @@
+import os
+
+# path to input file
+file = open(fr"{os.getcwd()}\Day3\memory_dump.txt")
+
+def scan_memory(file):
+    print(file)
+
+scan_memory(file)
+
+# ANSWER: ??? (varies with input file)
+
+
