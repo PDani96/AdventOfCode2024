eb0668c31f37c3dd2894cbde2bc2d9f809248a62
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2024-12-29 18:47:16 -0600

    Completed 3-1

--- Day3/three_one.py
+++ Day3/three_one.py
@@ -1,19 +1,33 @@
 import os
+import re
 
 # path to input file
 file = open(fr"{os.getcwd()}\memory_dump.txt")
 
 def scan_memory(file):
+    # placeholder for sum of instructions' results
+    result = 0
     # read input file into string
     instructions = file.read()
 
-    for c in instructions:
+    # define regex pattern
+    regex = r"mul\(\d{1,3},\d{1,3}\)"
+    # use regex to get a list of all instructions that match
+    uncorrupted_instructions = re.findall(regex, instructions)
 
+    # redefine regex pattern using grouping to easily extract the numbers to multiply
+    regex = r"(\d+),(\d+)"
+    # loop through all matches
+    for ui in uncorrupted_instructions:
+        # store grouped matches
+        numbers = re.search(regex, ui)
+        # multiply the first group from the match by the second group
+        result += int(numbers.group(1)) * int(numbers.group(2))
 
-    print(instructions)
+    print(result)
 
 scan_memory(file)
 
-# ANSWER: ??? (varies with input file)
+# ANSWER: 178538786 (varies with input file)
 
 
