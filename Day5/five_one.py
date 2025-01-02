feada35d077cbf1d5b54359ee80310cfa5b4dfea
Preston Danielson <101071990+PDani96@users.noreply.github.com>
2025-01-02 15:23:28 -0600

    Wrote pseudo-code for 5-1.

--- Day5/five_one.py
+++ Day5/five_one.py
@@ -3,11 +3,32 @@ import os
 # path to input file
 file = open(fr"{os.getcwd()}\updates.txt")
 
-def update_safety_manual(file):
+def verify_updates(file):
     # placeholder for adding up all the median pages
-    print(sum)
+    median_sum = 0
 
-update_safety_manual(file)
+    # read the file
+
+    # split file by page ordering rules and by updates
+
+    # store page ordering rules as a tuple of tuples
+
+    # store updates as a tuple of tuples
+
+    # iterate through page numbers in an update
+
+    # check if any rules exist for the current page number
+
+    # check if the current update has the other pages specified by the rules
+
+    # check that rules are satisfied
+
+    # update median sum with valid update
+    # median_sum +=
+
+    print(median_sum)
+
+verify_updates(file)
 
 # https://adventofcode.com/2024/day/5
 # ANSWER: ??? (varies with input file)
